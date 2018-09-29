6349680fbf94f20cbde8213dd8c1c159e2633ddb
jkuehner <jochen.kuehner@kardex.com>
2018-09-29 07:31:17 +0200

    bugfix error with not found image

--- SVGImage/SVGImage/SVG/Shapes/ImageShape.cs
+++ SVGImage/SVGImage/SVG/Shapes/ImageShape.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
+using System.IO;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Xml;
@@ -30,21 +29,30 @@ namespace SVGImage.SVG.Shapes
             string hRef = XmlUtil.AttrValue(node, "xlink:href", string.Empty);
             if (hRef.Length > 0)
             {
-                BitmapImage b = new BitmapImage();
-                b.BeginInit();
-                if (hRef.StartsWith("data:image/png;base64"))
+                try
                 {
-                    b.StreamSource = new System.IO.MemoryStream(Convert.FromBase64String(hRef.Substring("data:image/png;base64,".Length)));
+                    BitmapImage b = new BitmapImage();
+                    b.BeginInit();
+                    if (hRef.StartsWith("data:image/png;base64"))
+                    {
+                        b.StreamSource =
+                            new System.IO.MemoryStream(
+                                Convert.FromBase64String(hRef.Substring("data:image/png;base64,".Length)));
+                    }
+                    else
+                    {
+                        // filename given must be relative to the location of the svg file
+                        string svgpath = System.IO.Path.GetDirectoryName(svg.Filename);
+                        string filename = System.IO.Path.Combine(svgpath, hRef);
+                        if (File.Exists(filename))
+                            b.UriSource = new Uri(filename, UriKind.RelativeOrAbsolute);
+                    }
+
+                    b.EndInit();
+                    this.ImageSource = b;
                 }
-                else
-                {
-                    // filename given must be relative to the location of the svg file
-                    string svgpath = System.IO.Path.GetDirectoryName(svg.Filename);
-                    string filename = System.IO.Path.Combine(svgpath, hRef);
-                    b.UriSource = new Uri(filename, UriKind.RelativeOrAbsolute);
-                }
-                b.EndInit();
-                this.ImageSource = b;
+                catch(Exception ex)
+                { }
             }
         }
     }
