11f43c8cf34c09d61d2e9596f32c97eb7e29fae0
paulushub <paulushub@gmail.com>
2020-09-04 17:05:05 +0900

    ClipArtViewer: restoring the original sample for directory browsing.
    - Added the sample viewer, ClipArtViewer, with bug fixes and code improvements.
    - Added error logging to ClipArtViewer, some samples are throwing exceptions.
    - Modified other sample applications. Used tab control to split samples in Example.

---
+++ Samples/ClipArtViewer/DebugForm.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using SVGImage.SVG;
+using SVGImage.SVG.Shapes;
+
+namespace ClipArtViewer
+{
+	/// <summary>
+	/// Interaction logic for FullSizeForm.xaml
+	/// </summary>
+	public partial class DebugForm : UserControl
+	{
+		public static DependencyProperty SVGItemSourcePoperty = DependencyProperty.Register("SVGItemSource",
+			typeof(SVGItem),
+			typeof(DebugForm),
+			new FrameworkPropertyMetadata(null, new PropertyChangedCallback(OnSVGItemChanged)));
+		
+		static void OnSVGItemChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			((DebugForm)d).SetSVGItemSource(e.NewValue as SVGItem);
+		}
+
+		public SVGItem SVGItemSource
+		{
+			get { return (SVGItem)GetValue(SVGItemSourcePoperty); }
+			set { SetValue(SVGItemSourcePoperty, value); }
+		}
+
+		public void SetSVGItemSource(SVGItem svg)
+		{
+			if (svg == null || svg.SVGImage == null)
+            {
+				m_canvas.SetDrawing(null);
+				m_objTree.Populate(null);
+				return;
+			}
+
+			m_canvas.SetDrawing(svg.SVGImage);
+			m_objTree.Populate(svg.SVGRender.SVG);
+		}
+
+		public DebugForm()
+		{
+			InitializeComponent();
+
+            this.Loaded += OnDebugFormLoaded;
+		}
+
+        private void OnDebugFormLoaded(object sender, RoutedEventArgs e)
+        {
+			this.SetSVGItemSource(this.SVGItemSource);
+		}
+
+		private void OnSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+		{
+			Shape shape = m_objTree.SelectedShape;
+			if (shape != null)
+			{
+				m_canvas.ClearHighligh();
+				m_canvas.AddHighlight(SVGItemSource.SVGRender.CreateDrawing(shape));
+			}
+			else
+            {
+				m_canvas.ClearHighligh();
+            }
+		}
+	}
+}
---
+++ Samples/ClipArtViewer/SizeTypeForm.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClipArtViewer
+{
+	/// <summary>
+	/// Interaction logic for SizeTypeForm.xaml
+	/// </summary>
+	public partial class SizeTypeForm : UserControl
+	{
+		public static DependencyProperty ImageSourcePoperty = DependencyProperty.Register("ImageSource",
+			typeof(Drawing),
+			typeof(SizeTypeForm),
+			new FrameworkPropertyMetadata(null, new PropertyChangedCallback(OnImageSourceChanged)));
+		
+		static void OnImageSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			((SizeTypeForm)d).SetImage(e.NewValue as Drawing);
+		}
+		public Drawing ImageSource
+		{
+			get { return (Drawing)GetValue(ImageSourcePoperty); }
+			set { SetValue(ImageSourcePoperty, value); }
+		}
+		public void SetImage(Drawing drawing)
+		{
+			m_canvas1.SetImage(drawing);
+			m_canvas2.SetImage(drawing);
+			m_canvas3.SetImage(drawing);
+			m_canvas4.SetImage(drawing);
+			m_canvas5.SetImage(drawing);
+		}
+		public SizeTypeForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
